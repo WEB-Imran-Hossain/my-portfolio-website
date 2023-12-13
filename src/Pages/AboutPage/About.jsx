2e549f11d714cacccc990b2146f7152cfef4e57c
Imran <webimran2021@gmail.com>
2023-12-13 21:14:40 +0600

    added typerstyle

--- src/Pages/AboutPage/About.jsx
+++ src/Pages/AboutPage/About.jsx
@@ -2,11 +2,18 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 // ..
 AOS.init();
+import { useTypewriter} from "react-simple-typewriter";
 
 import about1 from '../../assets/images/Web Imran.jpg'
 import { FaUser } from "react-icons/fa";
 
 const About = () => {
+    const [typeEffect]=useTypewriter({
+        words: ['MERN Stack', 'Front-End', 'React'],
+        loop:{},
+        typeSpeed: 700,
+        delaySpeed: 40
+    })
     return (
   <div>
      <div className="hero min-h-screen my-10 mt-0">
@@ -30,17 +37,25 @@ const About = () => {
             </div>
           </div>
           <div className="relative ml-14">
-            <img
+         <div>
+         <img
               data-aos="fade-right"
               src={about1}
               className="lg:w-full rounded shadow-2xl"
             />
-            <iframe
+         </div>
+          <div>
+          <iframe
               data-aos="fade-left"
               src="https://lottie.host/embed/5e4ef915-95eb-4f1e-9c0f-9777afe21525/XEzZ7j8i8f.json"
               className="w-4/5 h-4/6 rounded shadow-2xl absolute  lg:top-24 lg:-left-64 top-28 -left-12 bottom-0 glass"
             />
           </div>
+        <div className="mt-3">
+        <h2 className="text-3xl font-Rajdhani text-center"><span>{typeEffect} Developer</span></h2>
+        </div>
+          </div>
+          
         </div>
       </div>
   </div>
